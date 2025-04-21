784f833d00e4bbacb2ffc2b2195ee29a2cc31c8d
emrdl7 <emrdl7@gmail.com>
2025-04-21 09:18:41 +0900

    수정

--- script.js
+++ script.js
@@ -32,3 +32,16 @@ selector.addEventListener("change", (e) => {
 
 // 초기 실행
 applyTheme();
+
+let lastTouchEnd = 0;
+document.addEventListener(
+  "touchend",
+  function (e) {
+    const now = Date.now();
+    if (now - lastTouchEnd <= 300) {
+      e.preventDefault(); // 더블탭 확대 방지
+    }
+    lastTouchEnd = now;
+  },
+  false
+);
